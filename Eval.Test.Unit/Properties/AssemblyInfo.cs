0d29c204659750f4a6765fa3979611e11222c5f8
Bjørnar Alvestad <bjornar.walle.alvestad@gmail.com>
2019-10-12 14:08:08 +0200

    Solution and project structure

---
+++ Eval.Test.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Eval.Test.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Eval.Test.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("15b407c9-2c45-42bd-9e6c-099bac0c9e4b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Eval.Test.Unit/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Eval.Test.Unit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var value = "Hello test world!";
+            value.Should().StartWith("Hello");
+        }
+    }
+}
