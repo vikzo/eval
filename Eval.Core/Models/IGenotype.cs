66f0c47054c21f8e6cbd04b4dcbfb3602387f9a1
Bjørnar Alvestad <bjornar.walle.alvestad@gmail.com>
2019-10-13 20:27:27 +0200

    Genotype interface, phenotype tests

---
+++ Eval.Core/Models/IGenotype.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eval.Core.Models
+{
+    public interface IGenotype
+    {
+        IGenotype Clone();
+        IGenotype CrossoverWith(IGenotype other, Crossover crossover);
+        void Mutate(double factor);
+    }
+}
